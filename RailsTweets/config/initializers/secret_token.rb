55f58d7ae8e1b5e0d6cc3531da90e4743dd6d947
ArturT <artur2006@o2.pl>
2012-06-20 22:39:55 +0200

    Default new rails project

---
+++ RailsTweets/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTweets::Application.config.secret_token = '872d7b12f516bf86f766c27d75469f245700c63fb1dab7ed6d8b735f3a8ee895230177db305f1bcac1fb88fc5a8c7102c7d271b4bb17189bab6485ad7df18353'
