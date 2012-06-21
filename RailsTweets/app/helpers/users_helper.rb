8fc92153cee3555193195c22d76c9ba456570c5d
ArturT <artur2006@o2.pl>
2012-06-21 18:32:50 +0200

    Added tweets helper. Updated view and css. Updated validation in user model.

--- RailsTweets/app/helpers/users_helper.rb
+++ RailsTweets/app/helpers/users_helper.rb
@@ -1,2 +1,31 @@
 module UsersHelper
+  
+  # Display gravatar as img tag
+  # http://stackoverflow.com/a/5043452
+  #
+  # @param [string] email 
+  # @param options {:alt, :class, :size} 
+  # @return [string] image_tag 
+  def gravatar_for email, options = {}
+    options = {:alt => 'avatar', :class => 'avatar', :size => 80}.merge! options
+    id = Digest::MD5::hexdigest email.strip.downcase
+    url = 'http://www.gravatar.com/avatar/' + id + '.jpg?s=' + options[:size].to_s
+    options.delete :size
+    image_tag url, options
+  end
+  
+  # Display recent user's tweets. You can define the number of tweets which will be shown.
+  # http://twitter.rubyforge.org/
+  #
+  # @param [string] twitter_login
+  # @param [integer] limit [optional] Default=3. Limit of recent tweets. 
+  # @return [string] Recent user's tweets.
+  def tweets twitter_login, limit = nil
+    limit = 3 if limit.nil? 
+            
+    Twitter.user_timeline(twitter_login, :rpp => limit, :result_type => "recent").map do |status|
+      "#{status.from_user}: #{status.text}"
+    end
+  end
+  
 end
--- RailsTweets/app/models/user.rb
+++ RailsTweets/app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :last_name, :name, :twitter_login
+  
+  validates_presence_of :email, :last_name, :name, :twitter_login
+      
+  validates_length_of :name, :maximum => 25
+    
+  validates_length_of :last_name, :maximum => 25
+  
+  validates :email, :uniqueness => true
+  validates_length_of :email, :maximum => 100
+  validates_format_of :email, :with => /\A[^@]{1,}@[^@]{1,}\.[a-zA-z]{2,}+\z/, :message => "is not valid"
+  
+  validates_format_of :twitter_login, :with => /\A[a-zA-Z]+\z/, :message => "only letters allowed"
+  validates_length_of :twitter_login, :maximum => 5 
 end
